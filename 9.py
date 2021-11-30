b1e11b799386a13e0c87f2c6bc43539cb37693ef
r-ashida <83977111+r-ashida@users.noreply.github.com>
2021-11-30 13:10:09 +0530

    Add files via upload

---
+++ 9.py
@@ -0,0 +1,10 @@
+#Write a program to count the number of zeroes in following tuple.
+# a = (7,0,8,0,0,9)
+
+def count(tup,x):
+   count==0
+   for ele in tup:
+      if(ele==x):
+        count=count+1
+
+      return count
