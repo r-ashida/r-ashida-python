33283d289a25779d4e47779af0407ef4d033efe0
r-ashida <83977111+r-ashida@users.noreply.github.com>
2021-11-30 13:10:50 +0530

    Add files via upload

---
+++ 27.py
@@ -0,0 +1,11 @@
+#Write a program to print the following star pattern
+ 
+def triangle(n):
+    k = n - 1
+    for i in range(0, n):
+        for j in range(0, k):
+            print(end=" ")
+        k = k - 1
+        for j in range(0, i+1):
+            print("* ", end="")
+        print("\r")
