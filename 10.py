6c3ee6a830a754d70cadca5c090026c55451c624
r-ashida <83977111+r-ashida@users.noreply.github.com>
2021-11-30 13:10:28 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,5 @@
+#Write a program to create a dictionary of Hindi words with values as 
+#their English Translation. Provide user with an option to look it up.
+
+form englisttohindi.englisttohindi import EngtoHindi
+message="i am rashida"
